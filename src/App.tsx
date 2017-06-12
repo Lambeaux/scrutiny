0581187078bf060f211bfe7e144ed47ec77cf071
Lombardi <steve.lombardi@connexta.com>
2017-06-12 09:36:12 -0700

    Core dependencies set and experimented with a table component

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,41 @@ import * as React from 'react';
 import './App.css';
 import Hello from './components/Hello';
 import Counter from './components/Counter';
+import ReportTable from './components/ReportTable';
+import MultiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Col, Grid, Row} from 'react-flexbox-grid';
 
 const logo = require('./logo.svg');
 
 class App extends React.Component<{}, null> {
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to React</h2>
+            <MultiThemeProvider>
+                <div className="App">
+                    <div className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h2>Welcome to React</h2>
+                    </div>
+                    <p className="App-intro">
+                        To get started, edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <Grid fluid>
+                        <Row>
+                            <Col xs={6}>
+                                <Hello name="TypeScript" enthusiasmLevel={10}/>
+                            </Col>
+                            <Col xs={6}>
+                                <Counter/>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={6} xsOffset={3}>
+                                <ReportTable/>
+                            </Col>
+                        </Row>
+                    </Grid>
                 </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <Hello name="TypeScript" enthusiasmLevel={10}/>
-                <Counter/>
-            </div>
+            </MultiThemeProvider>
         );
     }
 }
